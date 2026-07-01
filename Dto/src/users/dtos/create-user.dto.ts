fe315c07f26cac4bd6c863e618ee164090a1a9a4
marufbytes <marufahammed215@gmail.com>
2026-07-02 02:10:40 +0600

    Dto validation

---
+++ Dto/src/users/dtos/create-user.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator";
+
+export class createUserDto{
+
+    id:number;
+
+    @IsString({message: 'Name should be a string value.'})
+    @IsNotEmpty()
+    @MinLength(3, {message: 'Name must have at least 3 characters'})
+    name:string;
+
+    @IsString()
+    @IsOptional()
+    gender?: string;
+
+    @IsEmail()
+    email:string;
+
+    isMarried: boolean;
+}
