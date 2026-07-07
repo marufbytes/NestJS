3854f1cb3c3f678b6bc9759097ffb24a94b12e94
marufbytes <marufahammed215@gmail.com>
2026-07-08 03:19:55 +0600

    Not working properly

--- 9_Dto_Route_Parameter/src/users/users.controller.ts
+++ 9_Dto_Route_Parameter/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe, Body, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Post, Query, ParseBoolPipe } from "@nestjs/common";
+import { ValidationPipe, Body, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Post, Query } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { createUserDto } from "./dtos/create-user.dto";
 
@@ -8,14 +8,12 @@ export class UsersController {
     constructor(private readonly usersService: UsersService) {};
 
     //http://localhost:3000/users?limit=30&page=3
-    @Get(':isMarried')
+    @Get()
     getUsers(@Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit:number, 
-             @Query('page',new DefaultValuePipe(3), ParseIntPipe) page:number) 
-             @Param('isMarried', ParseBoolPipe) isMarried:boolean
-             {
-                console.log(isMarried);
+             @Query('page',new DefaultValuePipe(3), ParseIntPipe) page:number) {
+                console.log(limit,page);
                 return this.usersService.getAllUsers();
-             }
+    }
 
     @Get(':id')
     getUserByID(@Param('id', ParseIntPipe) id: number) {
